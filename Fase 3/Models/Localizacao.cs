3b0ac98de98768a4b31963ee701889435dcfe0da
Tourette137 <47273656+Tourette137@users.noreply.github.com>
2021-06-05 16:31:44 +0100

    Classes e Conexão da Base de Dados feita

---
+++ Fase 3/Models/Localizacao.cs
@@ -0,0 +1,18 @@
+﻿public class Localizacao
+{
+    public string Freguesia { get; set; }
+    public string Concelho { get; set; }
+    public string Distrito { get; set; }
+
+    public Localizacao()
+    {
+
+    }
+
+    public Localizacao(string freguesia, string concelho, string distrito)
+    {
+        this.Freguesia = freguesia;
+        this.Concelho = concelho;
+        this.Distrito = distrito;
+    }
+}
